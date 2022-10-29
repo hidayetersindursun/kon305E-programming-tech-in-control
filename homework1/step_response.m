c5174f9ec6aa32683bf1d1b5da7350d7e4f0ad78
Hidayet <h.e.dursun@gmail.com>
2022-10-29 20:44:56 +0300

    heun metodu eklendi

--- homework1/step_response.m
+++ homework1/step_response.m
@@ -4,8 +4,9 @@ function  [y,t]=step_response(Gs,method)
         num = cell2mat(num);
         den = cell2mat(den);
         [A,B,C,D] = tf2ss(num,den);
-        t_midpoint = 0:0.1:3;
-        
+        u = 1;
+        h = 0.1;
+        n = 2; % order of equation
         % default
         % default implementation goes here
         %if method == "default"
@@ -15,25 +16,35 @@ function  [y,t]=step_response(Gs,method)
         %% midpoint
         % midpoint implementation goes here
         %elseif method == "midpoint"           
-            u = 1;
-            h = 0.1;
+        
             t_midpoint = 0:0.1:3;
-            t = t_midpoint;
-            n = 2; % order of equation
-            x = zeros(n,length(t_midpoint));
+            x_midpoint = zeros(n,length(t_midpoint));
             y_midpoint = zeros(n-1,length(t_midpoint));
-            x(:,1) = 0;
+            x_midpoint(:,1) = 0;
             
             for i = 1:length(t_midpoint)
-                k1 = A*x(:,i)+ B*u;
-                k2 = A*(x(:,i)+ k1*h*0.5) + B*u;
-                x(:,i+1) = (x(:,i) + k2*h);
-                y_midpoint(i) = C*x(:,i+1) + D*u; 
+                k1_mid = A*x_midpoint(:,i)+ B*u;
+                k2_mid = A*(x_midpoint(:,i)+ k1_mid*h*0.5) + B*u;
+                x_midpoint(:,i+1) = (x_midpoint(:,i) + k2_mid*h);
+                y_midpoint(i) = C*x_midpoint(:,i+1) + D*u; 
             end
-
+        
         %% heun
         % heun implementation goes here
         %elseif method == "heun"
+        t_heun = 0:0.1:3;
+        x_heun = zeros(n,length(t_heun));
+        y_heun = zeros(n-1,length(t_heun));
+        x_heun(:,1) = 0;
+        
+            for i = 1:length(t_heun)
+                k1_heun = A*x_heun(:,i)+ B*u;
+                k2_heun = A*(x_heun(:,i) + k1_heun*h) + B*u;
+                x_heun(:,i+1) = (x_heun(:,i) + ((k1_heun/2)+(k2_heun/2))*h);
+                y_heun(i) = C*x_heun(:,i+1) + D*u;
+            end
+        
+            
         %% rkutta4
         % rkutta4 implementation goes here
         %elseif method == "rkutta4"
@@ -41,13 +52,27 @@ function  [y,t]=step_response(Gs,method)
         %else
             
         %end
-        x = 0:0.1:3;
-        x1 = linspace(0,3,length(y_default))
-  
-        plot(x,y_midpoint);
+        t_midpoint = 0:0.1:3;
+        t_default = linspace(0,3,length(y_default));
+         
+        plot(t_midpoint,y_midpoint);
         title("combine plots")
         hold on
-        plot(x1,y_default);
+        plot(t_default,y_default);
+        hold on
+        plot(t_heun,y_heun);
+        
+        if method == "default"
+            y = y_default;
+            t = t_default;
+        elseif method == "midpoint"           
+            y = y_midpoint;
+            t = t_midpoint;
+        elseif method == "heun"
+            y = y_heun;
+            t = t_heun;    
+        end
+
         % return of the result for given method method
 end
 
