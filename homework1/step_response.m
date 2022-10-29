8233aae404c750255fc34e4334bb8085b15c3e74
Hidayet <h.e.dursun@gmail.com>
2022-10-29 21:28:19 +0300

    rkutta3 eklendi

--- homework1/step_response.m
+++ homework1/step_response.m
@@ -1,5 +1,6 @@
 %% UPLOAD ONLY THIS FILE
 function  [y,t]=step_response(Gs,method)
+        
         [num,den] = tfdata(Gs);
         num = cell2mat(num);
         den = cell2mat(den);
@@ -7,51 +8,59 @@ function  [y,t]=step_response(Gs,method)
         u = 1;
         h = 0.1;
         n = 2; % order of equation
-        % default
+        
+        %% default
         % default implementation goes here
-        %if method == "default"
-            y_default = step(Gs,3);
-            stepinfo(Gs);
+        y_default = step(Gs,3);
+        stepinfo(Gs);
         
         %% midpoint
-        % midpoint implementation goes here
-        %elseif method == "midpoint"           
+        % midpoint implementation goes here          
         
-            t_midpoint = 0:0.1:3;
-            x_midpoint = zeros(n,length(t_midpoint));
-            y_midpoint = zeros(n-1,length(t_midpoint));
-            x_midpoint(:,1) = 0;
-            
-            for i = 1:length(t_midpoint)
-                k1_mid = A*x_midpoint(:,i)+ B*u;
-                k2_mid = A*(x_midpoint(:,i)+ k1_mid*h*0.5) + B*u;
-                x_midpoint(:,i+1) = (x_midpoint(:,i) + k2_mid*h);
-                y_midpoint(i) = C*x_midpoint(:,i+1) + D*u; 
-            end
+        t_midpoint = 0:0.1:3;
+        x_midpoint = zeros(n,length(t_midpoint));
+        y_midpoint = zeros(n-1,length(t_midpoint));
+        x_midpoint(:,1) = 0;
+
+        for i = 1:length(t_midpoint)
+            k1_mid = A*x_midpoint(:,i)+ B*u;
+            k2_mid = A*(x_midpoint(:,i)+ k1_mid*h*0.5) + B*u;
+            x_midpoint(:,i+1) = (x_midpoint(:,i) + k2_mid*h);
+            y_midpoint(i) = C*x_midpoint(:,i+1) + D*u; 
+        end
         
         %% heun
         % heun implementation goes here
-        %elseif method == "heun"
         t_heun = 0:0.1:3;
         x_heun = zeros(n,length(t_heun));
         y_heun = zeros(n-1,length(t_heun));
         x_heun(:,1) = 0;
         
-            for i = 1:length(t_heun)
-                k1_heun = A*x_heun(:,i)+ B*u;
-                k2_heun = A*(x_heun(:,i) + k1_heun*h) + B*u;
-                x_heun(:,i+1) = (x_heun(:,i) + ((k1_heun/2)+(k2_heun/2))*h);
-                y_heun(i) = C*x_heun(:,i+1) + D*u;
-            end
+        for i = 1:length(t_heun)
+            k1_heun = A*x_heun(:,i)+ B*u;
+            k2_heun = A*(x_heun(:,i) + k1_heun*h) + B*u;
+            x_heun(:,i+1) = (x_heun(:,i) + ((k1_heun/2)+(k2_heun/2))*h);
+            y_heun(i) = C*x_heun(:,i+1) + D*u;
+        end
         
-            
         %% rkutta4
         % rkutta4 implementation goes here
-        %elseif method == "rkutta4"
-        % plotting goes here
-        %else
-            
-        %end
+        t_rkutta4 = 0:0.1:3;
+        x_rkutta4 = zeros(n,length(t_rkutta4));
+        y_rkutta4 = zeros(n-1,length(t_rkutta4));
+        x_rkutta4(:,1) = 0;
+        
+        for i = 1:length(t_rkutta4)
+            k1_rkutta4 = A*x_rkutta4(:,i)+ B*u;
+            k2_rkutta4 = A*(x_rkutta4(:,i) + k1_rkutta4*h*0.5) + B*u;
+            k3_rkutta4 = A*(x_rkutta4(:,i) + k2_rkutta4*h*0.5) + B*u;
+            k4_rkutta4 = A*(x_rkutta4(:,i) + k3_rkutta4*h) + B*u;
+            x_rkutta4(:,i+1) = (x_rkutta4(:,i) + ((k1_rkutta4+2*k2_rkutta4+2*k3_rkutta4+k4_rkutta4)*h*(1/6)));
+            y_rkutta4(i) = C*x_rkutta4(:,i+1) + D*u;
+        end
+        
+        %% plotting goes here
+        
         t_midpoint = 0:0.1:3;
         t_default = linspace(0,3,length(y_default));
          
@@ -64,8 +73,14 @@ function  [y,t]=step_response(Gs,method)
         hold on
         
         plot(t_heun,y_heun,"b","LineWidth",1);
+        hold on
         
-        legend(' method=’default’, color=’k’ ',' method=’midpoint’, color=’r’ ',' method=’heun’, color=’b’ ');
+        plot(t_rkutta4,y_rkutta4,"m","LineWidth",1);
+        
+        legend(' method=’default’, color=’k’ ',' method=’midpoint’, color=’r’ ',' method=’heun’, color=’b’ ', ' method=’rkutta4’, color=’m’ ');
+        
+
+        %% return of the result for given method method
         if method == "default"
             y = y_default;
             t = t_default;
@@ -74,10 +89,11 @@ function  [y,t]=step_response(Gs,method)
             t = t_midpoint;
         elseif method == "heun"
             y = y_heun;
-            t = t_heun;    
+            t = t_heun;
+        elseif method == "rkutta4"
+            y = y_rkutta4;
+            t = t_rkutta4;
         end
-
-        % return of the result for given method method
 end
 
 %% Which method performs the best and why?
